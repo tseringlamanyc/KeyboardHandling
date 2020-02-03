4efef213b7ecb041e594c79161d85d4cd2ce23ac
Tsering Lama <tseringlama@pursuit.org>
2020-02-03 11:03:53 -0500

    using notification center to show the user if the keyboard shows up

--- KeyboardHandling/ViewController.swift
+++ KeyboardHandling/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var pursuitLogo: UIImageView!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var passWord: UITextField!
+    @IBOutlet weak var pursuitCenterYConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        registerForKeyBoardNotifications()
+    }
+    
+    private func registerForKeyBoardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func unregisterForKeyBoardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        print(notification.userInfo)
+        
+        guard let keyboardFrame = notification.userInfo["UIKeyboardFrameBeginUserInfoKey"] as? CGRect else {
+            return 
+        }
+    }
+    
+    @objc
+    private func keyboardWillHide(notification: NSNotification) {
+        print(notification.userInfo)
     }
 
 
